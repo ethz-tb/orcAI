570f4698ca5ff591b523060db4b62f788145d3b8
Daniel Angst <dcangst@gmail.com>
2025-02-19 12:14:00 +0100

    black formatting

--- src/orcAI/train.py
+++ src/orcAI/train.py
@@ -12,9 +12,11 @@ import orcAI.auxiliary as aux
 import orcAI.architectures as arch
 import orcAI.load as load
 
+
 # model parameters
 def count_params(trainable_weights):
     return np.sum([np.prod(w.shape) for w in trainable_weights])
+
 
 @click.command(
     help="Train a model on the given data",
@@ -22,42 +24,72 @@ def count_params(trainable_weights):
     no_args_is_help=True,
     epilog="For further information visit: https://gitlab.ethz.ch/seb/orcai_test",
 )
-@click.argument("data_dir",
-    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path),
+@click.argument(
+    "data_dir",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, resolve_path=True, path_type=Path
+    ),
 )
-@click.argument("output_dir",
-    type=click.Path(exists=False, file_okay=False, dir_okay=True, writable=True, resolve_path=True, path_type=Path),
+@click.argument(
+    "output_dir",
+    type=click.Path(
+        exists=False,
+        file_okay=False,
+        dir_okay=True,
+        writable=True,
+        resolve_path=True,
+        path_type=Path,
+    ),
 )
-@click.option("-m", "--model_parameter", "model_parameter_path",
+@click.option(
+    "-m",
+    "--model_parameter",
+    "model_parameter_path",
     help="Path to a JSON file containing model specifications",
     type=click.Path(exists=True, readable=True, resolve_path=True, path_type=Path),
     default=str(files("orcAI.defaults").joinpath("default_model_parameter.json")),
     show_default=True,
 )
-@click.option("-lc", "--label_calls", "label_calls_path",
+@click.option(
+    "-lc",
+    "--label_calls",
+    "label_calls_path",
     help="Path to a JSON file containing calls for labeling",
     type=click.Path(exists=True, readable=True, resolve_path=True, path_type=Path),
-    default=str(files("orcAI.defaults").joinpath("default_calls.json")), show_default=True,
+    default=str(files("orcAI.defaults").joinpath("default_calls.json")),
+    show_default=True,
 )
-@click.option("-lw", "--load_weights",
+@click.option(
+    "-lw",
+    "--load_weights",
     is_flag=True,
-    default=False, show_default=True,
+    default=False,
+    show_default=True,
     help="Load weights and continue fitting",
 )
-@click.option("-tp", "--transformer_parallel",
-    is_flag=True, default=False, show_default=True,
+@click.option(
+    "-tp",
+    "--transformer_parallel",
+    is_flag=True,
+    default=False,
+    show_default=True,
     help="Use transformer parallelization",
 )
-@click.option("-v", "--verbosity", type=click.IntRange(0, 1), default=0, show_default=True)
-def train(data_dir, output_dir,
-          model_parameter_path = files("orcAI.defaults").joinpath("default_model_parameter.json"),
-          label_calls_path = files("orcAI.defaults").joinpath("default_calls.json"),
-          load_weights = False,
-          transformer_parallel = False,
-          verbosity = 1):
-    """Trains an orcAI model
-    
-    """
+@click.option(
+    "-v", "--verbosity", type=click.IntRange(0, 1), default=0, show_default=True
+)
+def train(
+    data_dir,
+    output_dir,
+    model_parameter_path=files("orcAI.defaults").joinpath(
+        "default_model_parameter.json"
+    ),
+    label_calls_path=files("orcAI.defaults").joinpath("default_calls.json"),
+    load_weights=False,
+    transformer_parallel=False,
+    verbosity=1,
+):
+    """Trains an orcAI model"""
     # Initialize messenger
     msgr = aux.Messenger(verbosity=verbosity)
 
@@ -65,36 +97,42 @@ def train(data_dir, output_dir,
     msgr.info(f"Output directory: {output_dir}")
     msgr.info(f"Data directory: {data_dir}")
 
-    msgr.info("Loading parameter and data...", indent = 1)
+    msgr.info("Loading parameter and data...", indent=1)
     msgr.info("Model parameter")
-    
+
     model_parameter = aux.read_json(model_parameter_path)
     msgr.info(model_parameter)
-    model_name = model_parameter['name']
+    model_name = model_parameter["name"]
 
     label_calls = aux.read_json(label_calls_path)
     msgr.info("Calls for labeling")
-    msgr.info(label_calls, indent = -1)
+    msgr.info(label_calls, indent=-1)
 
     # TODO: Model Weights, dca had to make a change here, because of Keras API changes
     file_paths = {
-        'training_data': data_dir.joinpath("train_dataset"),
-        'validation_data': data_dir.joinpath("val_dataset"),
-        'test_data': data_dir.joinpath("test_dataset"),
-        'model': output_dir.joinpath(model_name, model_name+".h5"),
-        'model_dir': output_dir.joinpath(model_name),
-        'weights': output_dir.joinpath(model_name, model_name+".weights.h5"),
-        'history': output_dir.joinpath(model_name, "training_history.json"),
-        'confusion_matrices': output_dir.joinpath("confusion_matrices.json")
+        "training_data": data_dir.joinpath("train_dataset"),
+        "validation_data": data_dir.joinpath("val_dataset"),
+        "test_data": data_dir.joinpath("test_dataset"),
+        "model": output_dir.joinpath(model_name, model_name + ".h5"),
+        "model_dir": output_dir.joinpath(model_name),
+        "weights": output_dir.joinpath(model_name, model_name + ".weights.h5"),
+        "history": output_dir.joinpath(model_name, "training_history.json"),
+        "confusion_matrices": output_dir.joinpath("confusion_matrices.json"),
     }
 
     # load data sets from local disk
-    msgr.info(f"Loading train, val and test datasets from {data_dir}", indent = 1)
+    msgr.info(f"Loading train, val and test datasets from {data_dir}", indent=1)
     tf.config.set_soft_device_placement(True)
     start_time = time.time()
-    train_dataset = load.reload_dataset(file_paths['training_data'], model_parameter['batch_size'])
-    val_dataset = load.reload_dataset(file_paths['validation_data'], model_parameter['batch_size'])
-    test_dataset = load.reload_dataset(file_paths['test_data'], model_parameter['batch_size'])
+    train_dataset = load.reload_dataset(
+        file_paths["training_data"], model_parameter["batch_size"]
+    )
+    val_dataset = load.reload_dataset(
+        file_paths["validation_data"], model_parameter["batch_size"]
+    )
+    test_dataset = load.reload_dataset(
+        file_paths["test_data"], model_parameter["batch_size"]
+    )
     msgr.info(f"time to load datasets: {time.time() - start_time:.2f} seconds")
 
     # Verify the val dataset and obtain shape
@@ -121,9 +159,7 @@ def train(data_dir, output_dir,
                     name="masked_binary_accuracy",
                 )
                 model = arch.build_cnn_res_transformer_model(
-                    input_shape,
-                    num_labels,
-                    **model_parameter
+                    input_shape, num_labels, **model_parameter
                 )
                 model.compile(
                     optimizer="adam",
@@ -138,41 +174,49 @@ def train(data_dir, output_dir,
     msgr.part("Compiling model: " + model_name)
     if load_weights:
         msgr.info("Loading weights from stored model: " + model_name)
-        model.load_weights(file_paths['weights'])
+        model.load_weights(file_paths["weights"])
     else:
         msgr.info("Learning weights from scratch")
 
     # Metrics
     masked_binary_accuracy_metric = tf.keras.metrics.MeanMetricWrapper(
         fn=lambda y_true, y_pred: arch.masked_binary_accuracy(
-            y_true, y_pred, **model_parameter['masked_binary_accuracy_metric']
+            y_true, y_pred, **model_parameter["masked_binary_accuracy_metric"]
         ),
         name="masked_binary_accuracy",
     )
     masked_f1_metric = tf.keras.metrics.MeanMetricWrapper(
         fn=lambda y_true, y_pred: arch.masked_f1_score(
-            y_true, y_pred, **model_parameter['masked_f1_metric']
+            y_true, y_pred, **model_parameter["masked_f1_metric"]
         ),
         name="masked_f1_score",
     )
 
     # Callbacks
     early_stopping = EarlyStopping(
-        monitor=model_parameter['callback_metric'],  # val_masked_binary_accuracy | val_masked_f1_score
-        patience=model_parameter['patience'],  # Number of epochs to wait for improvement
+        monitor=model_parameter[
+            "callback_metric"
+        ],  # val_masked_binary_accuracy | val_masked_f1_score
+        patience=model_parameter[
+            "patience"
+        ],  # Number of epochs to wait for improvement
         mode="max",  # Stop when accuracy stops increasing
         restore_best_weights=True,  # Restore weights from the best epoch
-        verbose=verbosity
+        verbose=verbosity,
     )
     model_checkpoint = ModelCheckpoint(
-        file_paths['weights'],
-        monitor=model_parameter['callback_metric'],  # val_masked_binary_accuracy | val_masked_f1_score
+        file_paths["weights"],
+        monitor=model_parameter[
+            "callback_metric"
+        ],  # val_masked_binary_accuracy | val_masked_f1_score
         save_best_only=True,
         save_weights_only=True,
-        verbose=verbosity
+        verbose=verbosity,
     )
     reduce_lr = ReduceLROnPlateau(
-        monitor=model_parameter['callback_metric'],  # val_masked_binary_accuracy | val_masked_f1_score
+        monitor=model_parameter[
+            "callback_metric"
+        ],  # val_masked_binary_accuracy | val_masked_f1_score
         factor=0.5,  # Reduce learning rate by a factor of 0.5
         patience=3,  # Wait for 3 epochs of no improvement
         min_lr=1e-6,  # Set a lower limit for the learning rate
@@ -189,7 +233,7 @@ def train(data_dir, output_dir,
     total_params = model.count_params()
     trainable_params = count_params(model.trainable_weights)
     non_trainable_params = count_params(model.non_trainable_weights)
-    msgr.info("Model size:", indent = 1)
+    msgr.info("Model size:", indent=1)
     msgr.info(f"Total parameters: {total_params}")
     msgr.info(f"Trainable parameters: {trainable_params}")
     msgr.info(f"Non-trainable parameters: {non_trainable_params}")
@@ -203,7 +247,7 @@ def train(data_dir, output_dir,
         history = model.fit(
             train_dataset,
             validation_data=val_dataset,
-            epochs=model_parameter['epochs'],
+            epochs=model_parameter["epochs"],
             callbacks=[early_stopping, model_checkpoint, reduce_lr],
             verbose=verbosity,
         )
@@ -211,11 +255,11 @@ def train(data_dir, output_dir,
 
     msgr.info(f"training history: {history.history}")
     msgr.info(f"saving training history: {file_paths['history']}")
-    with open(file_paths['history'], "w") as f:
+    with open(file_paths["history"], "w") as f:
         f.write(str(history.history))
 
     # Model evaluation
-    msgr.info(f"Evaluating model: {model_name}", level = "part")
+    msgr.info(f"Evaluating model: {model_name}", level="part")
     test_loss, test_metric = model.evaluate(test_dataset)
     msgr.info(f"test loss: {test_loss}")
     msgr.info(f"test masked binary accuracy: {test_metric}")
@@ -235,17 +279,24 @@ def train(data_dir, output_dir,
     confusion_matrices = aux.compute_confusion_matrix(
         y_true_batch, y_pred_batch, label_calls, mask_value=-1
     )
-    msgr.info(f"confusion matrices:", indent = 1)
+    msgr.info(f"confusion matrices:", indent=1)
     msgr.print_confusion_matrices(confusion_matrices)
     arch.masked_binary_accuracy(y_true_batch, y_pred_batch, mask_value=-1.0)
-    aux.write_json(confusion_matrices, file_paths['confusion_matrices'])
+    aux.write_json(confusion_matrices, file_paths["confusion_matrices"])
     msgr.print_dict(confusion_matrices)
 
-    aux.write_json(model_parameter, file_paths['model_dir'].joinpath("model_parameter.json"))
-    aux.write_json(label_calls, file_paths['model_dir'].joinpath("trained_calls.json"))
-    aux.write_json({"input_shape": input_shape, "num_labels": num_labels}, file_paths['model_dir'].joinpath("shape.json"))
+    aux.write_json(
+        model_parameter, file_paths["model_dir"].joinpath("model_parameter.json")
+    )
+    aux.write_json(label_calls, file_paths["model_dir"].joinpath("trained_calls.json"))
+    aux.write_json(
+        {"input_shape": input_shape, "num_labels": num_labels},
+        file_paths["model_dir"].joinpath("shape.json"),
+    )
     # TODO: Save model?
-    model.save(file_paths['model'])
+    model.save(file_paths["model"])
 
-    msgr.success(f"OrcAI - training model finished. Model saved to {file_paths['model']}")
+    msgr.success(
+        f"OrcAI - training model finished. Model saved to {file_paths['model']}"
+    )
     return
