4b4a7f78dbe57317360f6f630cefd3e77e26d2ff
Daniel Angst <dcangst@gmail.com>
2025-03-03 17:30:11 +0100

    remove non package files

--- Scripts/create_spec.sh
+++
@@ -1,1 +0,0 @@
-MakeData/create_spectrogram.py -c laptop -p /Users/sb/polybox/Documents/Research/Sebastian/OrcAI_project/ -w all_annotated_files
--- Scripts/create_tvtdata.sh
+++
@@ -1,1 +0,0 @@
-MakeData/create_tvtdata.py -c laptop  -p .  -m cnn_res_lstm_model
