5f324a0af40968204771566eda64c56c7469fbd0
Daniel Angst <daniel.angst@env.ethz.ch>
2025-04-22 11:35:57 +0200

    improve docstring

--- src/orcAI/auxiliary.py
+++ src/orcAI/auxiliary.py
@@ -394,7 +394,7 @@ def filter_filepaths(
 
 
 def seconds_to_hms(seconds: int) -> str:
-    """convert seconds to h:m:s format"""
+    """convert seconds to hh:mm:ss format"""
     hours, remainder = divmod(seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
     return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
