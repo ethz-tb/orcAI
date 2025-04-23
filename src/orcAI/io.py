8dd6c78dfcdd4d757abd7f7f5389d2c5b33fa3ef
Daniel Angst <daniel.angst@env.ethz.ch>
2025-04-23 08:42:41 +0200

    handle np.float32 in JsonEncoderExt

--- src/orcAI/io.py
+++ src/orcAI/io.py
@@ -200,10 +200,19 @@ def read_json(filename):
         dictionary = json.load(file)
     return dictionary
 
+class JsonEncoderExt(json.JSONEncoder):
+    """Custom JSON encoder to handle additional data types."""
+
+    def default(self, obj):
+        if isinstance(obj, Path):
+            return str(obj)
+        if isinstance(obj, np.float32):
+            return obj.astype(np.float64)
+        return super().default(obj)
 
 def write_json(dictionary, filename):
     """write dictionary into json file"""
-    json_string = json.dumps(dictionary, indent=4)
+    json_string = json.dumps(dictionary, indent=4, cls=JsonEncoderExt)
     with open(filename, "w") as file:
         file.write(json_string)
     return
